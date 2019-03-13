82cefc08747975fe62c4beb45ae574d5e7216bb7
Alan Villa <a19i23@hotmail.com>
2019-03-12 20:56:50 -0700

    added logic to make disney blackout dates work.

--- src/main/java/org/alanvilla/projects/disneyRestJava/BlackoutDates.java
+++ src/main/java/org/alanvilla/projects/disneyRestJava/BlackoutDates.java
@@ -11,17 +11,19 @@ public class BlackoutDates {
 	private static int date = calendar.get(Calendar.DATE);
 	private static int month = calendar.get(Calendar.MONTH) + 1;
 	
-	private static HashMap<Integer, ArrayList<Integer>> disneyBlackoutDates = new HashMap<Integer, ArrayList<Integer>>() {
+	private static HashMap<Integer, ArrayList<Integer>> disneyDeluxeBlackoutDates = new HashMap<Integer, ArrayList<Integer>>() {
 		{
-			put(1, new ArrayList<Integer>(Arrays.asList(1,2,3,4,5)));		//January
-		    put(2, new ArrayList<Integer>(Arrays.asList(16, 17)));			//February
-		    put(3, new ArrayList<Integer>(Arrays.asList(16, 23, 30))); 		//March
+			put(1, new ArrayList<Integer>(Arrays.asList(1,2,3,4,5)));			//January
+		    put(2, new ArrayList<Integer>(Arrays.asList(16, 17)));				//February
+		    put(3, new ArrayList<Integer>(Arrays.asList(16, 23, 30))); 			//March
+		    put(4, new ArrayList<Integer>(Arrays.asList(6, 13, 19, 20, 27))); 	//April
+
 
 		}
 	};
 	
 	public static boolean isDisneyBlackoutDate() {
-		ArrayList<Integer> datesForMonth = disneyBlackoutDates.get(month); 
+		ArrayList<Integer> datesForMonth = disneyDeluxeBlackoutDates.get(month); 
 		
 		if(datesForMonth.contains(date)) {
 			return true;
--- src/main/java/org/alanvilla/projects/disneyRestJava/ResultsResource.java
+++ src/main/java/org/alanvilla/projects/disneyRestJava/ResultsResource.java
@@ -15,28 +15,51 @@ import javax.ws.rs.core.MediaType;
 @Path("results")
 public class ResultsResource {
 	
+	enum ThemePark{
+		DISNEYLAND, CALIFORNIA_ADVENTURE;
+	}
+	
 	public static final String DISNEY_RESULT_JSON = "Disney result JSON";
 	public static final String CALIFORNIA_ADVENTURE_RESULT_JSON = "California Adventure result JSON";
+	public static final String TITLE_TEXT = "titleText";
+	public static final String MAIN_TEXT = "mainText";
+	public static final String RIDERECT_URL = "redirectionUrl";
+	public static final String URL = "https://alanvilla.com";
 
 
+
+
+	/**
+	 * Get request for results endpoint that returns a json array of two 
+	 * objects. First one is the disneyland object and second is
+	 * California adventure object
+	 * @return
+	 */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonArray getProperties() {
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
         JsonObjectBuilder builder = Json.createObjectBuilder();
 
-        arrayBuilder.add(buildAlexaObject(builder)); 
+        arrayBuilder.add(buildAlexaObject(builder, ThemePark.DISNEYLAND));
+        arrayBuilder.add(buildAlexaObject(builder, ThemePark.CALIFORNIA_ADVENTURE)); 
         return arrayBuilder.build();
     }
     
-    JsonObjectBuilder buildAlexaObject (JsonObjectBuilder builder) { 	
+    JsonObjectBuilder buildAlexaObject (JsonObjectBuilder builder, ThemePark park) { 	
     	addUTCData(builder);
-    	builder.add("titleText", DISNEY_RESULT_JSON);  
-    	if (BlackoutDates.isDisneyBlackoutDate()) {
-    		
-    	} else {
-    		
-    	}
+    	if (park == ThemePark.DISNEYLAND) {
+        	builder.add(TITLE_TEXT, DISNEY_RESULT_JSON);
+        	if (BlackoutDates.isDisneyBlackoutDate()) {
+        		builder.add(MAIN_TEXT, "Disneyland is not available today.");
+        	} else {
+        		builder.add(MAIN_TEXT, "Disneyland is available today.");
+        	}
+        	
+    	} else if (park == ThemePark.CALIFORNIA_ADVENTURE){
+        	builder.add(TITLE_TEXT, CALIFORNIA_ADVENTURE_RESULT_JSON);  
+    	}	
+    	builder.add(RIDERECT_URL, URL);
 		return builder;   	
     }
     
